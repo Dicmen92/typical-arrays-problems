f28e6a6dae7f6870601149c572569041a0aa43a4
Dicmen92 <58484766+Dicmen92@users.noreply.github.com>
2021-03-14 20:17:53 +0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,43 +1,23 @@
-
 exports.min = function min (array) {
-  if(array === undefined || array.length === 0) {
+  if (array === undefined || array.length === 0) {
     return 0;
   }
-
-  var minimum = array.reduce(function (startingValue, currentValue) {
-    if(startingValue < currentValue) {
-      return startingValue;
-    } else {
-      return currentValue;
-    }
-  });
-  return minimum;
-};
-
-
-exports.max = function max (array) {
-  if(array === undefined || array.length === 0) {
-  return 0;
+  return Math.min.apply(Math,array);
 }
 
-const maximum = array.reduce(function (startingValue, currentValue) {
-  if(startingValue > currentValue) {
-    return startingValue;
-  } else {
-    return currentValue;
-  }
-});
-return maximum;
-};
-
-
-exports.avg = function avg (array) {
-  if(array === undefined || array.length === 0) {
+exports.max = function max (array) {
+  if (array === undefined || array.length === 0) {
     return 0;
   }
+  return Math.max.apply(Math,array);
+}
 
-  var sum = array.reduce(function (startingValue, currentValue) {
-    return startingValue + currentValue;
-  });
-  return sum / array.length;
-};
+exports.avg = function avg (array) {
+  if (array === undefined || array.length === 0) {
+    return 0;
+  }
+  
+  let res = 0
+  for (let i=0; i < array.length;i++){
+    res += array[i];} 
+    return res/array.length;}
